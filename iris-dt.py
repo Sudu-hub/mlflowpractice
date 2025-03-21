12ccf5eea4afe62b8e5af6f1db28371aa1365c84
sudu-hub <sudarshansahane1044@gmail.com>
2025-03-21 18:38:44 +0530

    updation

--- iris-dt.py
+++ iris-dt.py
@@ -16,12 +16,16 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 max_depth = 15
 n_estimators = 100
 
+# to set experiment to track experiment
 mlflow.set_experiment('iris-dt')
+
+#run mlflow
 with mlflow.start_run():
     dt = DecisionTreeClassifier(max_depth=max_depth)
     dt=dt.fit(X_train, y_train)
     y_pred = dt.predict(X_test)
     accuracy = accuracy_score(y_test, y_pred)
+    #we use metrics
     mlflow.log_metric('accuracy',accuracy)
     mlflow.log_param('max_depth',max_depth)
     #mlflow.log_param('n_estimator',n_estimators)
